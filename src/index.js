c1005f8a479369bfdcf78980d3f944160c2ecb27
HawiCaesar <brian.odhiambo@andela.com>
2019-04-17 15:44:36 +0300

    feat(setup): add resolvers, schemas, prettier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-import "dotenv/config";
+import 'dotenv/config';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors'
 
-console.log("We live and ready");
-console.log(process.env.MY_SECRET);
+const app = express();
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String
+    age: Int
+  }
+`;
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Brian Hawi',
+        age: 25
+      };
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({port: 8000}, () =>  {
+  console.log("We live and ready at http://localhost:8000/graphql")
+})
